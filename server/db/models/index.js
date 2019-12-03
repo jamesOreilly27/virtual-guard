053dee2abf873e926a8b95399c73a5be08915a46
James O'Reilly <James.Oreilly926@gmail.com>
2019-12-03 16:44:46 -0500

    add associations

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -4,4 +4,21 @@ const PermanentGuest = require('./permanentGuest')
 const Community = require('./community')
 const ServiceTicket = require('./serviceTicket')
 
+/********** Associations **********/
+
+/*** Resident to Community ***/
+Community.hasMany(Resident, { as: 'residents' })
+Resident.belongsTo(Community)
+
+/*** Resident to PermanentGuests ***/
+Resident.hasMany(PermanentGuest, { as: 'guests' })
+PermanentGuest.belongsTo(Resident)
+
+/*** ServiceTickets ***/
+ServiceTicket.belongsTo(Community)
+Community.hasMany(ServiceTicket, { as: 'tickets' })
+
+ServiceTicket.belongsTo(Agent)
+Agent.hasMany(ServiceTicket, { as: 'tickets' })
+
 module.exports = { Resident, Agent, PermanentGuest, Community, ServiceTicket }
