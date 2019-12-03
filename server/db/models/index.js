0b98ba42e586eb4d36999ad415614c24f59200c4
James O'Reilly <James.Oreilly926@gmail.com>
2019-12-03 14:06:19 -0500

    add community model to the database

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -1,5 +1,6 @@
 const Resident = require('./resident')
 const Agent = require('./agent')
 const PermanentGuest = require('./permanentGuest')
+const Community = require('./community')
 
-module.exports = { Resident, Agent, PermanentGuest }
+module.exports = { Resident, Agent, PermanentGuest, Community }
