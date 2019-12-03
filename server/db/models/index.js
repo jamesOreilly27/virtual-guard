3b2f27c2adb106599c29c3c9cfc4fb3323a71259
James O'Reilly <James.Oreilly926@gmail.com>
2019-12-03 13:58:27 -0500

    add the permanent guest model to the databas

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -1,4 +1,5 @@
 const Resident = require('./resident')
 const Agent = require('./agent')
+const PermanentGuest = require('./permanentGuest')
 
-module.exports = { Resident, Agent }
+module.exports = { Resident, Agent, PermanentGuest }
