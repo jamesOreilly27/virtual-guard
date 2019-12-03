3cb43e435a700f12862e9f5af4d91a09d15205a2
James O'Reilly <James.Oreilly926@gmail.com>
2019-12-03 14:13:17 -0500

    add ServiceTicket model to the database

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -2,5 +2,6 @@ const Resident = require('./resident')
 const Agent = require('./agent')
 const PermanentGuest = require('./permanentGuest')
 const Community = require('./community')
+const ServiceTicket = require('./serviceTicket')
 
-module.exports = { Resident, Agent, PermanentGuest, Community }
+module.exports = { Resident, Agent, PermanentGuest, Community, ServiceTicket }
