d1d2116bc6c698b1cb1c8cf23ab7486063f48732
James O'Reilly <James.Oreilly926@gmail.com>
2019-12-03 21:32:04 -0500

    add an isOnline property to the Agent model

--- server/db/models/agent.js
+++ server/db/models/agent.js
@@ -5,7 +5,8 @@ const Agent = db.define('agent', {
   firstName: { type: Sequelize.STRING, allowNull: false },
   lastName: { type: Sequelize.STRING, allowNull: false },
   ticketCount: { type: Sequelize.INTEGER, allowNull: false, defaultValue: 0 },
-  isAdmin: { type: Sequelize.BOOLEAN, allowNull: false, defaultValue: false }
+  isAdmin: { type: Sequelize.BOOLEAN, allowNull: false, defaultValue: false },
+  isOnline: { type: Sequelize.BOOLEAN, allowNull: false, defaultValue: false }
 })
 
 module.exports = Agent
